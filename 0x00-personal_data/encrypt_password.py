7c8946549020deec6db7c97fa2343251db54072e
Rivashjerrier <rivijerrier@gmail.com>
2023-08-03 18:23:39 +0000

    Encrypting passwords

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+encrypt_password.py module
+"""
+
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """
+    Returns a salted, hashed password, which is a byte string.
+    """
+    hashed = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+    return hashed
