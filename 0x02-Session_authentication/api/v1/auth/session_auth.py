d2b4c60e372c4407979c1f30282fa3f4c94de513
Rivashjerrier <rivijerrier@gmail.com>
2023-08-10 19:26:22 +0000

    Empty session

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+session_auth.py module
+"""
+
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """
+    Class SessionAuth that inherits from Auth
+    """
