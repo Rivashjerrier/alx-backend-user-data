59ad3c4f1934ceea3a3e1038b5ef41dc1b49228c
Rivashjerrier <rivijerrier@gmail.com>
2023-08-17 20:51:20 +0000

    Log out

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -54,7 +54,7 @@ def logout():
     """
     session_id = request.cookies.get("session_id")
     user = AUTH.get_user_from_session_id(session_id)
-    if session_id is None or user is None:
+    if user is None:
         abort(403)
     AUTH.destroy_session(user.id)
     return redirect("/")
