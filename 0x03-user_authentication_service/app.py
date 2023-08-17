fa974b4a8daf5504fab03610d4966fa2747ab0b5
Rivashjerrier <rivijerrier@gmail.com>
2023-08-17 22:36:08 +0000

    Get reset password token

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -80,7 +80,7 @@ def get_reset_password_token():
     try:
         email = request.form.get("email")
         reset_token = AUTH.get_reset_password_token(email)
-        return jsonify({"email": email, "reset_token": token})
+        return jsonify({"email": email, "reset_token": reset_token})
     except ValueError:
         abort(403)
 
