14006a872f6df721c0fa5df05e24807386888b20
Rivashjerrier <rivijerrier@gmail.com>
2023-08-17 19:10:29 +0000

    Generate UUIDs

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -18,6 +18,13 @@ def _hash_password(password: str) -> bytes:
     return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
 
 
+def _generate_uuid() -> str:
+    """
+    Returns a string representation of a new UUID
+    """
+    return str(uuid.uuid4())
+
+
 class Auth:
     """Auth class to interact with the authentication database.
     """
@@ -53,9 +60,3 @@ class Auth:
         except NoResultFound:
             return False
         return False
-
-    def _generate_uuid(self) -> str:
-        """
-        Returns a string representation of a new UUID
-        """
-        return str(uuid.uuid4())
